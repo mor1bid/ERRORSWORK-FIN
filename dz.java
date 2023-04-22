01877f164d52387a508ef1947f96f96d50b2e10d
De <spark49942@gmail.com>
2023-04-22 06:28:11 +0300

    errorme-12

--- dz.java
+++ dz.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.FileWriter;
 
-/**
- * dz
- */
 public class dz {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
         boolean here = true;
         Scanner work = new Scanner(System.in);
@@ -21,9 +21,20 @@ public class dz {
             }
             else 
             {
-                here = false;
+                for (int i = 0; i<userdata.length; i++) 
+                {
+                    if(userdata[i] instanceof String) 
+                    {
+                        FileWriter storage = new FileWriter(new File(userdata[i] + ".txt"), StandardCharsets.UTF_8, true);
+                        for (int j = 0; i<userdata.length; j++)
+                        {
+                            storage.write(j);
+                        }
+                    }
+                }
+                // here = false;
             }
-    }
+        }
 
     }
 }
