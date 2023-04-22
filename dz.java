dd759a7062e9c5d26f9f159d0a451cb2cfe72be3
De <spark49942@gmail.com>
2023-04-22 19:39:43 +0300

    errorwork-f

--- dz.java
+++ dz.java
@@ -15,26 +15,37 @@ public class dz {
             System.out.println(" Введите, пожалуйста, через пробел свои ФИО, дату рождения(дд.мм.гг), номер телефона и пол:");
             String data = work.nextLine();
             String[] userdata = data.split(" ");
-            if (userdata.length != 6) 
+            if (data.matches("[0-9]+") || data.matches("[a-zA-Z]+")) 
             {
-                System.out.println("Обработка прервана. Вам нужно заполнить данную форму строго по установленным пунктам. Попробуйте ещё раз!");
+                System.out.println("Данные не приняты. Введёное вами не имеет смыслового значения. Попробуйте ещё раз!");
             }
-            else 
+            else if (userdata.length != 6)
+            {
+                System.out.println("Обработка прервана. Вам нужно заполнить данную форму СТРОГО по установленным пунктам. Попробуйте ещё раз!");
+            }
+            else
             {
                 for (int i = 0; i<userdata.length; i++) 
                 {
                     if(userdata[i] instanceof String) 
                     {
                         FileWriter storage = new FileWriter(new File(userdata[i] + ".txt"), StandardCharsets.UTF_8, true);
-                        for (int j = 0; i<userdata.length; j++)
+                        for (int j = 0; j < userdata.length; j++)
                         {
-                            storage.write(j);
+                            storage.write("<");
+                            storage.write(userdata[j]);
+                            storage.write(">");
                         }
+                        storage.write("\n");
+                        i = userdata.length;
+                        storage.flush();
+                        storage.close();
+                        here = false;
                     }
                 }
-                // here = false;
             }
         }
-
+        System.out.println("Запись файла успешно выполнена!");
+        work.close();
     }
 }
