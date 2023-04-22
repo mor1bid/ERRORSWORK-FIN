26b3deec43afd9dd4fe917bf5eb3db8fd99bcf60
De <spark49942@gmail.com>
2023-04-23 00:08:24 +0300

    errorwork-f

--- dz.java
+++ dz.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 import java.nio.charset.StandardCharsets;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class dz {
 
     public static void main(String[] args) throws Exception
     {
+        Pattern ern = Pattern.compile("[^A-Za-z0-9 ]");
         boolean here = true;
         Scanner work = new Scanner(System.in);
         System.out.println("Здравствуйте!");
         while (here) 
         {
-            System.out.println(" Введите, пожалуйста, через пробел свои ФИО, дату рождения(дд.мм.гг), номер телефона и пол:");
+            System.out.println(" Введите, пожалуйста, через пробел свои ФИО, дату рождения(дд.мм.гггг без точек), номер телефона (только числа) и пол:");
             String data = work.nextLine();
+            Matcher mat = ern.matcher(data);
             String[] userdata = data.split(" ");
-            if (data.matches("[0-9]+") || data.matches("[a-zA-Z]+")) 
+            if (data.matches("[0-9]+") || data.matches("[a-zA-Z]+") || mat.find() ) 
             {
-                System.out.println("Данные не приняты. Введёное вами не имеет смыслового значения. Попробуйте ещё раз!");
+                System.out.println("Данные не приняты. Введёное вами не имеет смыслового значения или содержит специальные символы. Попробуйте ещё раз!");
             }
             else if (userdata.length != 6)
             {
